475ad2785491bccb7d36622caa19c73f33bc18d0
zengshu <zengshu@xiaomi.com>
2016-12-09 14:36:58 +0800

    新增数据库帮助类,以及各表对应的实体类

---
+++ app/src/main/java/com/coolweather/app/model/City.java
@@ -0,0 +1,45 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by zengshu on 16-12-9.
+ */
+
+public class City {
+
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/coolweather/app/model/County.java
@@ -0,0 +1,45 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by zengshu on 16-12-9.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+}
+
